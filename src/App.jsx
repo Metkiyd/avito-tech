fcc3d4580b6b545e358f1593b2f1d051f0b8abc3
Metkiyd <sviridenko.dev@gmail.com>
2022-11-07 23:04:36 +0300

    router init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Routes, Route} from 'react-router-dom';
+import { Main } from './Components/Main/Main'
+import { Header } from './Components/Header/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        
-      </header>
-    </div>
-  );
-}
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route exact path="/" element={<Main />} />
+			</Routes>
+		</>
+	)
+};
 
 export default App;
